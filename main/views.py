47c0a9ccd4e4dbee5b32a54eb92788c46779665d
Daria <80263493+dgridina@users.noreply.github.com>
2021-10-20 19:46:35 +0200

    finish basic recipe HTML page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from main.models import Recipe
 
 
 def get_recipe(request, recipe_id):
-    recipe = Recipe.objects.get(id=recipe_id)
+    recipe = get_object_or_404(Recipe, id=recipe_id)
     context = {
         'recipe': recipe
     }
