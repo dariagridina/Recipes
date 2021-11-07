d1d68d2727c5570623895995197c74d58c08f8ec
Daria <dasha.gridina.98@gmail.com>
2021-11-07 19:10:10 +0100

    registration form added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import views
 from django.urls import path
 
-from accounts.views import CustomLoginView
+from accounts.views import CustomLoginView, RegistrationView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('signup/', RegistrationView.as_view(), name='sign_up'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
 
 
 class CustomLoginView(LoginView):
     redirect_authenticated_user = True
+
+
+class RegistrationView(CreateView):
+    template_name = 'registration/sign_up.html'
+    model = User
+    form_class = UserCreationForm
+    success_url = '/recipe'
